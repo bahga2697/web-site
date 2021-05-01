b43ba64b33bea9d7fbc1d900a7f13100e5a6430f
bahga2697 <https://github.com/bahga2697/my-project.git>
2021-05-01 08:55:36 -0700

    first commit

---
+++ test.js
@@ -0,0 +1,21 @@
+// var siteName=document.getElementById("siteName");
+// var siteUrl=document.getElementById("siteUrl");
+// document.getElementById("myForm").addEventListener("submit",saveBookmark);
+// var bookmarks;
+// function saveBookmark(){
+//     var bookmark={
+//         name:siteName,
+//         Url:siteUrl
+//     }
+//     bookmarks.push(bookmark);
+//     console.log(bookmark);
+//     localStorage.setItem("test","hello world");
+
+// }
+// saveBookmark();
+var siteName=document.getElementById('siteName');
+var siteUrl=document.getElementById("siteUrl");
+var mainBtn=document.getElementById("mainBtn");
+
+ 
+
